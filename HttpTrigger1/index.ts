d295545ebc3648fe3b71be7f1d40f8b877321358
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-11-03 23:45:19 +0900

    fix: v2

--- HttpTrigger1/index.ts
+++ HttpTrigger1/index.ts
@@ -2,7 +2,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const responseMessage = "v1"
+    const responseMessage = "v2"
 
     context.res = {
         // status: 200, /* Defaults to 200 */
