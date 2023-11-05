b74ecb70fa910513d22b77a68e415fa0edd353f2
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-11-05 13:04:01 +0900

    fix: CosmosDBトリガ関数v6

--- CosmosTrigger1/index.ts
+++ CosmosTrigger1/index.ts
@@ -1,11 +1,13 @@
 import { AzureFunction, Context } from "@azure/functions"
 
 const cosmosDBTrigger: AzureFunction = async function (context: Context, documents: any[]): Promise<void> {
-    const version = "v5";
+    const version = "v6";
+    const invocationId = context.invocationId;
+    const instanceId = process.env["WEBSITE_INSTANCE_ID"];
 
     if (!!documents && documents.length > 0) {
         const documentId = documents[0].id;
-        context.log(`Function Version: ${version} Target Document Id: ${documentId}`);
+        context.log(`Function Version: ${version} InvovationID: ${invocationId} InstanceId: ${instanceId} Target Document Id: ${documentId}`);
     }
 }
 
